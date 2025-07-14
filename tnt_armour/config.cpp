9c9327b128d3ea0d6187382a9ada2d30d8be8993
sweetn3ss <40582849+sweetn3ss@users.noreply.github.com>
2025-07-13 21:29:33 -0700

    toast's drip, pngs will be removed soon

--- tnt_armour/config.cpp
+++ tnt_armour/config.cpp
@@ -49,6 +49,41 @@ class cfgWeapons {
 			mass = 40;
         };
     };
+    class tnt_jedi_uniform_toast: tnt_armour_trooper {
+        author = "Toast";
+        displayName = "[293rd] Jedi Uniform (Toast)";
+        class ItemInfo: UniformItem {
+            uniformModel = "-";
+            uniformClass = "tnt_jedi_uniform_toast_F";
+            containerClass = "Supply150";
+            mass = 25;
+        };
+    };
+    class tnt_mask_toast: NVGoggles {
+		scope=2;
+		scopeArsenal=2;
+		author="Toast";
+		displayName="[293rd] Master Toast's Mask";
+		model="tnt_armour\data\Jedi_Mask_Toast.p3d";
+		class ItemInfo
+		{
+			type=616;
+			hmdType=0;
+			uniformModel="tnt_armour\data\Jedi_Mask_Toast.p3d";
+			modelOff="tnt_armour\data\Jedi_Mask_Toast.p3d";
+			mass=2;
+		};
+	};
+    class tnt_hood_toast: swlb_clone_p1_helmet {
+        author = "Toast";
+        displayName = "[293rd] Master Toast Hood";
+        model = "tnt_armour\data\Jedi_Hood_Toast.p3d";
+        class ItemInfo: HeadgearItem {
+            modelSides[] = {6};
+            mass = 10;
+            uniformModel = "tnt_armour\data\Jedi_Hood_Toast.p3d";
+        };
+    };
 };
 class cfgVehicles {
     class ls_clone_phase2_base;
@@ -59,5 +94,12 @@ class cfgVehicles {
 		uniformClass = "tnt_armour_trooper";
 		hiddenSelections[] = {"camo1","camo2","undersuit"};
 		hiddenSelectionsTextures[] = {"tnt_armour\data\tnt_armour_upper_CO.paa","tnt_armour\data\tnt_armour_lower_CO.paa","tnt_armour\data\tnt_armour_undersuit_CO.paa"};
+    };
+    class tnt_jedi_uniform_toast_F: tnt_armour_trooper_F {
+        author = "Toast";
+        scope = 1;
+        displayName = "-";
+        uniformClass = "tnt_jedi_uniform_toast";
+        model = "tnt_armour\data\jedi_uniform_toast.p3d";
     };
 };
